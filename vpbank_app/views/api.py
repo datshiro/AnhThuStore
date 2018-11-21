3e5e79533b8027e48c5f2c0b2da72153b4dde55d
Dat Shiro <datnq@pacaliaasia.com>
2018-11-21 22:44:48 +0700

    Clean duplicate semantic-ui library Build another VPB-Bank

--- vpbank_app/views/api.py
+++ vpbank_app/views/api.py
@@ -8,7 +8,7 @@ from common.constants import BankCertificates, CertificateOwner, CertificateType
 from common.messages import Messages, ErrorMessages
 from core.module import Module
 from models.bank_session import BankSession
-from models.vcb_card import VCBBank, DoesNotExist
+from models.vpb_card import VPBBank, DoesNotExist
 from services.cipher import decrypt_rsa
 from services.converter import json as custom_json
 from services.keys import get_key
@@ -21,7 +21,7 @@ def authorization():
     data = request.form.to_dict()
 
     session_id = data.get('session_id')
-    kuis = get_key(CertificateOwner.VCB_BANK, CertificateType.BANK)['public_key']
+    kuis = get_key(CertificateOwner.VPB_BANK, CertificateType.BANK)['public_key']
     b64_kuis = base64.b64encode(kuis)
 
     authdata = data.get('authdata')
@@ -30,21 +30,21 @@ def authorization():
         pi = json.loads(pi)
         try:
             card_id = pi.get('card_id')
-            card = VCBBank.objects.get(pk=card_id)
+            card = VPBBank.objects.get(pk=card_id)
             if card:
                 bank_session = BankSession(id=session_id)
                 bank_session.data = pi
                 bank_session.save()
 
                 return make_response(custom_json(
-                    {'authresponse': Messages.AUTH_RESPONSE, 'bankcertificate': BankCertificates.VCB,
+                    {'authresponse': Messages.AUTH_RESPONSE, 'bankcertificate': BankCertificates.VPB,
                      'b64_kuis': b64_kuis.decode()}))
         except DoesNotExist:
             return make_response(custom_json(
-                {'authresponse': ErrorMessages.INVALID_CARD, 'bankcertificate': BankCertificates.VCB, 'b64_kuis': b64_kuis.decode()}))
+                {'authresponse': ErrorMessages.INVALID_CARD, 'bankcertificate': BankCertificates.VPB, 'b64_kuis': b64_kuis.decode()}))
 
     return make_response(custom_json(
-        {'authresponse': ErrorMessages.INVALID_AUTH_REQUEST, 'bankcertificate': BankCertificates.VCB, 'b64_kuis': b64_kuis.decode()}))
+        {'authresponse': ErrorMessages.INVALID_AUTH_REQUEST, 'bankcertificate': BankCertificates.VPB, 'b64_kuis': b64_kuis.decode()}))
 
 
 @module.get_post('/password')
@@ -61,14 +61,14 @@ def password():
     pwd_kuisencrypted = base64.b64decode(b64_pwd_kuisencrypted)
 
     # Decrypt pwd_kuisencrypted
-    kris = RSA.importKey(get_key(CertificateOwner.VCB_BANK, CertificateType.BANK)['private_key'])
+    kris = RSA.importKey(get_key(CertificateOwner.VPB_BANK, CertificateType.BANK)['private_key'])
     pwd = decrypt_rsa(kris, pwd_kuisencrypted)
 
     bank_session = BankSession.objects.get(pk=session_id)
     card_id = bank_session.data.get('card_id')
 
     try:
-        card = VCBBank.objects.get(pk=card_id, password=pwd.decode())
+        card = VPBBank.objects.get(pk=card_id, password=pwd.decode())
     except DoesNotExist:
         return make_response(custom_json({'payment_response': ErrorMessages.FAILED_VERIFY_TRANSACTION}))
 
--- webapp/static/js/checkout.js
+++ webapp/static/js/checkout.js
@@ -35,192 +35,7 @@ function concateArray(a, b) {
     return c;
 }
 
-
-function makePurchaseRequest(oi) {
-    var data = "Hello World";
-    var enc = new TextEncoder(); // always utf-8
-    var array_data = enc.encode(data);
-    var iv = window.crypto.getRandomValues(new Uint8Array(16));
-    iv_hex = arrayToHex(iv);
-    var ciphertext_array = "";
-    var plaintext_array = "";
-    var key_hex = "";
-    var ciphertext_hex = "";
-    function encrypt() {
-        window.crypto.subtle.generateKey(
-            {
-                name: "AES-CBC",
-                length: 256, //can be  128, 192, or 256
-            },
-            true, //whether the key is extractable (i.e. can be used in exportKey)
-            ["encrypt", "decrypt"] //can be "encrypt", "decrypt", "wrapKey", or "unwrapKey"
-        )
-            .then(function (k) {
-                key = k;
-            })
-            .then(function () {
-                window.crypto.subtle.encrypt(
-                    {
-                        name: "AES-CBC",
-                        iv: iv,
-                    },
-                    key, //from generateKey or importKey above
-                    array_data //ArrayBuffer of data you want to encrypt
-                )
-                    .then(function (encrypted) {
-                        //returns an ArrayBuffer containing the encrypted data
-                        ciphertext_array = new Uint8Array(encrypted);
-                        console.log("Ciphertext: ", ciphertext_array);
-                        ciphertext_hex = arrayToHex(ciphertext_array);
-                        return ciphertext_hex;
-                    })
-                    .then(function (result) {
-                        window.crypto.subtle.exportKey(
-                            "raw", //can be "jwk" or "raw"
-                            key //extractable must be true
-                        )
-                            .then(function (keydata) {
-                                var bufView = new Uint8Array(keydata);
-                                key_array = bufView;
-                                console.log("Key: ", key_array);
-                                key_hex = arrayToHex(key_array);
-
-                            })
-                    })
-                    .then(function (result) {
-                        decrypt()
-                    })
-                    .then(function (result) {
-                        // Create Dual Signature
-                        oi_array = encode_info(order_info);
-                        pi_array = encode_info(payment_info)
-                        var pimd = "";
-                        var oimd = "";
-                        var pomd = "";
-
-                        window.crypto.subtle.digest(
-                            {
-                                name: "SHA-256",
-                            },
-                            oi_array //The data you want to hash as an ArrayBuffer
-                        )
-                            .then(function (hash) {
-                                oimd = new Uint8Array(hash);
-                            })
-                            .then(function (result) {
-                                window.crypto.subtle.digest(
-                                    {
-                                        name: "SHA-256",
-                                    },
-                                    pi_array //The data you want to hash as an ArrayBuffer
-                                )
-                                    .then(function (hash) {
-                                        pimd = new Uint8Array(hash);
-                                        return pimd;
-                                    })
-                                    .then(function (result) {
-                                        console.log(result);
-                                        po = concateArray(oimd, pimd);
-                                        window.crypto.subtle.digest(
-                                            {
-                                                name: "SHA-256",
-                                            },
-                                            po //The data you want to hash as an ArrayBuffer
-                                        )
-                                            .then(function (hash) {
-                                                pomd = new Uint8Array(hash);
-                                                pomd = arrayToHex(pimd)
-                                                oimd = arrayToHex(oimd)
-                                                pimd = arrayToHex(pimd)
-                                                console.log("POMD: ", pomd);
-                                                return pomd;
-                                            })
-                                            .then(function (result) {
-                                                console.log("R: ", result);
-                                            })
-                                            .catch(function (err) {
-                                                console.error(err);
-                                            });
-                                    })
-                            })
-                            .catch(function (err) {
-                                console.error(err);
-                            });
-
-                    })
-                    .then(function (result) {
-                        console.log("RESULT: ", result);
-                    })
-                    .catch(function (err) {
-                        console.error(err);
-                    });
-            })
-            .catch(function (err) {
-                console.error(err);
-            });
-    }
-
-    function decrypt() {
-        console.log("K: ", key);
-        console.log("iv", iv);
-        console.log("ciphertext", ciphertext_array);
-        window.crypto.subtle.decrypt(
-            {
-                name: "AES-CBC",
-                iv: iv, //The initialization vector you used to encrypt
-            },
-            key, //from generateKey or importKey above
-            ciphertext_array //ArrayBuffer of the data
-        )
-            .then(function (decrypted) {
-                //returns an ArrayBuffer containing the decrypted data
-                console.log("Decrypted data: " + new Uint8Array(decrypted));
-                console.log(new Uint8Array(decrypted));
-                plaintext_array = new Uint8Array(decrypted);
-                var enc = new TextDecoder("utf-8");
-
-                console.log("Decrypted decoded-data: " + enc.decode(new Uint8Array(decrypted)));
-
-            })
-            .catch(function (err) {
-                console.error(err);
-            });
-    }
-    setTimeout(encrypt, 100);
-    //    setTimeout(decrypt,500);
-    // Export Key
-
-    var card_id = $("input[name=card-id]").val();
-    setTimeout(function () {
-        $.ajax({
-            url: '/api/make_purchase_request',
-            data: {
-                "key": key_hex,
-                "iv": iv_hex,
-                "ciphertext": ciphertext_hex,
-                "card-id": card_id
-            },
-            dataType: "json",
-            type: 'POST',
-            success: function (response) {
-                if (response.data.status == "NO") {
-                    alert("Không thanh toán đươc, hãy kiểm tra lại thông tin!");
-                    //                    finished();
-                    return false;
-                } else {
-                    alert("Cám ơn " + " đã mua hàng. Chúng tôi sẽ liên lạc để xác nhận và giao hàng nhanh nhất trong thời gian tới!");
-                    //                    window.location.href = response.data.url;hang
-                    //                    finished();
-                }
-            },
-            error: function (error) {
-                console.log("error");
-                //                finished();
-            }
-        });
-    }, 2000);
-}
-
+var order_id = $("#order-id").text();
 var order_id = $("#order-id").text();
 var order_sum = $('#order-sum').text();
 var order_info = {
@@ -710,6 +525,7 @@ function send_password(password, kuis) {
             var authdata_and_hashed_k6encrypted = "";
             var kupg = $('#kupg').val();
             var kum = $('#kum').val();
+            var bank_name = $("select[name=bank-name]").val();
 
             // Encrypt Password
             encrypt_rsa(kuis, password).then(function (encrypted) {
@@ -778,6 +594,7 @@ function send_password(password, kuis) {
                                                                         "iv6": arrayToHex(iv6),
                                                                         "authdata_and_hashed_k6encrypted": arrayToHex(authdata_and_hashed_k6encrypted),
                                                                         "pwd_kuisencrypted_and_hashed_k6encrypted": arrayToHex(pwd_kuisencrypted_and_hashed_k6encrypted),
+                                                                        "bank_name": bank_name
                                                                     },
                                                                     dataType: "json",
                                                                     type: 'POST',
